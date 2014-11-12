943c9116cb6a223e0833fcbadec3019a1938ed30
Tim Howard <tghoward@gmail.com>
2014-11-12 16:20:40 -0500

    add comments to sweave loop

--- CreatingMetadata/RunSweaveExportOnManyModels.r
+++ CreatingMetadata/RunSweaveExportOnManyModels.r
@@ -1,14 +1,17 @@
-
-setwd("C:/_NYNHP/GIS_data/ElementData/RdataFiles/build_eval_Out")
+##
+# This script will loop through a folder containing many SDMs
+# and create metadata output for each
+##
 
 library(ROCR)  #July 2010: order matters, see http://finzi.psych.upenn.edu/Rhelp10/2009-February/189936.html
 library(randomForest)
 
+#begin by setting the folder containing the SDM .RData files
+setwd("C:/_NYNHP/GIS_data/ElementData/RdataFiles/build_eval_Out")
 ##get a list of what's in the directory
 d <- dir(pattern = ".Rdata",full.names=FALSE)
 
-#
-##loop through
+##loop through all the Rdata files and create sweave output for each
 for (sweavei in 1:length(d)){
 		fileName <- d[[sweavei]]
   ## Bring the file into R
@@ -25,3 +28,4 @@ for (sweavei in 1:length(d)){
 	rm(list=ls()[!ls() %in% c("sweavei","d")])
 
 }
+
